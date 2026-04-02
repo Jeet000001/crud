607de12b0bc94a1af6f7e636be12d26c215fcb79
Jeet Paul <jeetpaul2222@gmail.com>
2026-04-02 22:50:08 +0530

    Implement GET method in task route: fetch tasks from database and handle errors

--- app/api/task/route.js
+++ app/api/task/route.js
@@ -25,3 +25,29 @@ export const POST = async (req) => {
     );
   }
 };
+
+export const GET = async () => {
+  try {
+    await dbConnect();
+    const Tasks = await Tasks.find({}).sort({
+      createdAt: -1,
+    });
+    // find({}) = No filter get all teh Data & .sort({ createdAt: -1 }) = short the notes base on create time ans descending order (newest first)
+
+    return NextResponse.json(
+      {
+        success: true,
+        data: tasks,
+      },
+      { status: 200 },
+    );
+  } catch (error) {
+    return NextResponse.json(
+      {
+        success: false,
+        error: error.message,
+      },
+      { status: 500 },
+    );
+  }
+};
